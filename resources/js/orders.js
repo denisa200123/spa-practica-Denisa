8f360439de1181b71852ef6e4faa9fb8e034d267
Olaru Denisa <denisa.olaru179@gmail.com>
2024-11-07 15:38:53 +0200

    changed date from orders according to iso formats

--- resources/js/orders.js
+++ resources/js/orders.js
@@ -2,24 +2,25 @@ window.renderOrders = function(orders) {
     let html = [
         '<tr>',
             '<th>Order id</th>',
-            '<th>Date</th>',
             '<th>Customer name</th>',
             '<th>Contact details</th>',
             '<th>Comments</th>',
             '<th>Total price</th>',
+            '<th>Date</th>',
             '<th>Products</th>',
         '</tr>'
     ].join('');
 
     $.each(orders, function (key, order) {
+        let date = new Date(order.created_at).toISOString().split('T')[0];
         html += [
             '<tr>',
                 '<td>' + order.id + '</td>',
-                '<td>' + order.created_at + '</td>',
                 '<td>' + order.customer_name + '</td>',
                 '<td>' + order.contact_details + '</td>',
                 '<td>' + order.comments + '</td>',
                 '<td>' + order.total_price + '</td>',
+                '<td>' + date + '</td>',
                 '<td><a href=#order/' + order.id + '>See products</a></td>',
             '</tr>'
         ].join('');
